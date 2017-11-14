fbd09d6e9ee0fa06bb723c48869c200ebd572d36
anknife <1981856494@qq.com>
2017-11-14 22:20:01 +0800

    basic

--- src/games/basic_model.vue
+++ src/games/basic_model.vue
@@ -2,16 +2,20 @@
 <div>
   <span id="title-first" v-if="!if_start">基本模式</span>
   <el-button id="start" v-if="!if_start" @click="makeNumber" type="primary">游戏开始</el-button>
+  <span id="title-two" v-if="if_start">基本猜测</span>
   <div class="main" v-if="if_start">
     <div class="middle">
-      <div class="number" v-if="!show_answer">{{num}}</div>
-      <el-input v-model="ipt" placeholder="请输入猜测的数字" id="gue-ipt" @keyup.enter.native="guessNumber">
-        <el-button slot="append" @click="guessNumber">确定</el-button>
+      <div class="number" v-if="show_answer">游戏答案：{{num}}</div>
+      <el-tooltip effect="dark" content="点击会使本次游戏失败" placement="bottom" :disabled="show_answer">
+        <el-button class="show" @click="show_answer = true,game_over = true" type="primary" v-if="!show_answer">查看答案</el-button>
+      </el-tooltip>
+      <el-input v-model="ipt" placeholder="请输入猜测的数字" id="gue-ipt" @keyup.enter.native="guessNumber" :disabled="show_answer">
+        <el-button slot="append" @click="guessNumber" :disabled="show_answer">确定</el-button>
       </el-input>
-      <div class="guide">提示:{{msg}}</div>
-      <div class="guess-num">当前猜测次数：{{n}}</div>
+      <div class="guide">游戏提示：{{msg}}</div>
+      <div class="guide">猜测次数：{{n}}&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</div>
       <el-row :gutter="20" class="gg-btn" v-if="game_over">
-        <el-col :span="12"><el-button @click="show_answer = true" type="primary">{{over_msg}}</el-button></el-col>
+        <el-col :span="12"><el-button type="primary">查看排行</el-button></el-col>
         <el-col :span="12"><el-button @click="makeNumber">重新开始</el-button></el-col>
       </el-row>
     </div>
@@ -23,7 +27,6 @@
   export default {
     data () {
       return {
-        over_msg: '',
         show_answer: false,
         game_over: false,
         n: 0,
@@ -74,7 +77,6 @@
           } else {
             x = 4
             y = 0
-            this.over_msg = '查看排行'
             this.game_over = true
             this.show_answer = true
             this.$alert('恭喜！游戏成功，已为您查看排行榜信息', '成功', {
@@ -89,7 +91,6 @@
           this.msg = x + 'A' + y + 'B'
         } else if (this.n === 16) {
           this.game_over = true
-          this.over_msg = '查看答案'
           this.$alert('您已经连续猜了16次，游戏结束', '失败', {
             confirmButtonText: '确定'
           }).then(() => {
@@ -106,9 +107,16 @@
 
 <style scoped>
   #title-first{
-    text-align: center;
     position: absolute;
     top: 100px;
+    left: 50%;
+    transform: translateX(-50%);
+    font-size: 30px;
+    color: #878D99;
+  }
+  #title-two{
+    position: absolute;
+    top: 20px;
     left: 50%;
     transform: translateX(-50%);
     font-size: 30px;
@@ -122,18 +130,29 @@
    transform: translateX(-40px);
  }
  .number{
-   width: 50%;
-   background-color: #EDF2FC;
+   margin-left: 50%;
+   transform: translateX(-50%);
+   height: 35px;
+ }
+ .show{
+   margin-left: 50%;
+   transform: translateX(-180%);
+   width: 100px;
+   height: 35px;
  }
  .main{
    text-align: center;
  }
  .middle{
-   margin-top: 250px;
-   transform: translateY(-50%);
+   margin-top: 100px;
  }
   #gue-ipt{
     width: 50%;
+    margin-top: 10px;
+  }
+  .guide{
+    height: 50px;
+    line-height: 50px;
   }
   .gg-btn{
     width: 50%;
