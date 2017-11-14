04c10d7cecee7e3d16c907445f1b8f886506e6ec
anknife <1981856494@qq.com>
2017-11-14 18:05:34 +0800

    sign

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,10 @@
 <template>
 <div id="main">
-  <div class="sign"><router-link to="/home/game/basic">Go to game</router-link></div>
+  <div id="back">
+    <img src="../img/star.png" id="my-img">
+    <span class="title">Guess Numbers</span>
+    <router-link to="/home/game/basic"><span class="sign">Go to game</span></router-link>
+  </div>
 </div>
 </template>
 
@@ -15,10 +19,37 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .sign{
+  #back{
+    position:fixed;
+    width:100%;
+    height:100%;
+    background-color: #108ee9;
+  }
+  #my-img{
     position: absolute;
-    top: 300px;
+    top: 150px;
     margin-left: 50%;
     transform: translateX(-50%);
+  }
+  .title{
+    font-size: 40px;
+    color: #fff;
+    position: absolute;
+    top: 280px;
+    margin-left: 50%;
+    transform: translateX(-50%);
+  }
+  .sign{
+    font-size: 25px;
+    color: #fff;
+    position: absolute;
+    top: 350px;
+    margin-left: 50%;
+    transform: translateX(-50%);
+    transition: all .3s linear;
+  }
+  .sign:hover{
+    color: #67C23A;
+    font-size: 30px;
   }
 </style>
